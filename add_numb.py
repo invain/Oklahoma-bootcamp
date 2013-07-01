85f394c6ae3403f30bfb81ca72b040dd400db5ea
invain <panyujie@gmail.com>
2013-07-01 15:21:45 -0500

    Added John as a co-author

--- add_numb.py
+++ add_numb.py
@@ -1,5 +1,6 @@
 # This has been authored by Yujie but who know who will attribute to it in the future
 # from adding import add_two, add_three
+# Added John as a co-author
 
 def main():
    print("2+2+2 = ",add_three(2,2,2))
