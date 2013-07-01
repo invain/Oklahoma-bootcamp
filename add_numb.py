43d56c5e3d846370b06d059e8744fe55e1bd240c
invain <panyujie@gmail.com>
2013-07-01 14:29:45 -0500

    Added module with functions

--- add_numb.py
+++ add_numb.py
@@ -1,8 +1,8 @@
 # This has been authored by Yujie but who know who will attribute to it in the future
-def add_two(a,b):
-   return a+b
+# from adding import add_two, add_three
 
 def main():
+   print("2+2+2 = ",add_three(2,2,2))
    print("2 + 3 = ",add_two(2,3))
 
 if __name__ == "__main__":
